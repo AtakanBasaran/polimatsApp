cf2b353aee023080216212e2a5a067c541352863
Atakan Başaran <basaranatakan10@gmail.com>
2024-04-04 18:59:06 +0300

    Initial Commit

---
+++ Polimats-GitHub/Polimats_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polimats_GitHubApp.swift
+//  Polimats-GitHub
+//
+//  Created by Atakan Başaran on 4.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Polimats_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
